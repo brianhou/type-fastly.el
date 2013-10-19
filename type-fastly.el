f6cae9ec7428e63d93c1b63fc4ae9a18ad1678b0
brianhou <brian.hou@live.com>
2013-10-19 08:44:38 -0700

    It works! It's fragile, but it works.

--- type-fastly.el
+++ type-fastly.el
@@ -8,26 +8,30 @@
 (defun read-file (name)
   "Prompts for file name and returns the file as a string."
   (interactive `(,(ido-read-file-name "Find file: ")))
-  (get-file-string (expand-file-name name)))
-
-(defvar text "hello there!")
+  (defvar type-fastly-text
+    (get-file-string (expand-file-name name))))
 
 (defun first-char (string) (string-to-char string))
 (defun rest-char (string) (substring string 1 nil))
 (defun fastly-insert-char ()
   (interactive)
-  (if (not (= (length text) 0))
-      (progn (insert-char (first-char text) 1) (setq text (rest-char text)))
-      (progn (message "Done!"))))
+  (if (not (= (length type-fastly-text) 0))
+      (progn
+        (insert-char (first-char type-fastly-text) 1)
+        (setq type-fastly-text (rest-char type-fastly-text)))
+      (message "Done.")))
 
+;;;###autoload
 (define-minor-mode type-fastly-mode
   "Fake your code presentations."
-  :lighter " f"
+  :lighter " *"
   :keymap (let ((map (make-sparse-keymap)))
             (define-key map [remap self-insert-command] 'fastly-insert-char)
-            (define-key map (kbd "+") 'type-fastly-mode)
-            map))
+            (define-key map (kbd "<S-escape>") 'type-fastly-mode)
+            map)
+  (if type-fastly-mode
+      (call-interactively 'read-file)))
 
 (global-set-key (kbd "<f8>") 'type-fastly-mode) ; for debugging
 
-(message "Reloaded type-fastly.el")
+(provide 'type-fastly)
